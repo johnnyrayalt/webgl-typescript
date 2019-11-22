a9d81e37dfcb8051920d84c9a7dd47698a7a2417
Johnny Ray Alt <johnnyrayalt@gmail.com>
2019-11-21 17:34:43 -0800

    class for extracting rgb color values and converts to xyz

---
+++ src/Core/utilities/ConvertRBGToXYZ.ts
@@ -0,0 +1,9 @@
+export class ConvertRbgToXyz {
+	public static extractRBGValues = (): number[] => {
+		let r: number = parseInt((<HTMLInputElement>document.getElementById('r')).value) / 100;
+		let g: number = parseInt((<HTMLInputElement>document.getElementById('g')).value) / 100;
+		let b: number = parseInt((<HTMLInputElement>document.getElementById('b')).value) / 100;
+
+		return [r, g, b];
+	};
+}
