5034a14be4fc0e6cd1ce45aa1eecaf1a0720b768
Johnny Ray Alt <johnnyrayalt@gmail.com>
2019-11-25 12:16:26 -0800

    redraws buffer on window resize from checkRender method on canvas class

--- src/Core/Engine.ts
+++ src/Core/Engine.ts
@@ -24,20 +24,40 @@ export class Engine {
 
 		this.createBuffer();
 
-		GLCanvas.resize(this.canvas);
 		this.loop();
 	};
 
+	/**
+	 * Main loop that is called 60 times a second
+	 */
 	private loop = (): void => {
-		gl.clear(gl.COLOR_BUFFER_BIT);
+		/**
+		 * Clears canvas for new draw
+		 */
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-		// Set uniforms.
+		/**
+		 * Rerender canvas if resized
+		 */
+		GLCanvas.checkRender(this.canvas, this.buffer);
+
+		/**
+		 * Sets viewport, i.e. where in clipspace the object is rendered
+		 */
+		gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+
+		/**
+		 * Sets uniforms from shaders
+		 */
 		const colorPosition = this.shader.getUniformLocation('u_color');
 		const colorValues = ConvertRbgToXyz.extractRBGValues();
 		gl.uniform4f(colorPosition, colorValues[0], colorValues[1], colorValues[2], colorValues[3]);
 
+		/**
+		 * Binds shader values
+		 */
 		this.buffer.bind();
-		this.buffer.draw();
+
 
 		requestAnimationFrame(this.loop.bind(this));
 	};
