1a86df3f9c4036d294a136ec9ca3749715b5ecac
Johnny Ray Alt <johnnyrayalt@gmail.com>
2019-11-25 14:26:10 -0800

    remove console.log

--- src/Core/Utilities/GLSLWrapper.ts
+++ src/Core/Utilities/GLSLWrapper.ts
@@ -9,7 +9,6 @@ export class GLSLWrapper {
 	};
 
 	public static convertFilesToString = (filePaths: string[]): string[] => {
-		console.log(filePaths)
 		const filesAsStrings: string[] = [];
 		filePaths.forEach(filePath => {
 			const fileAsString = require(`../${filePath}`).default;
