17098ceec7344fbf526c5e441af1e98dd7077622
Johnny Ray Alt <johnnyrayalt@gmail.com>
2019-11-26 12:03:02 -0800

    comments and remove floats from DOM response values

--- src/Core/Services/ConvertRBGToXYZ.ts
+++ src/Core/Services/ConvertRBGToXYZ.ts
@@ -8,6 +8,14 @@ export class ConvertRbgToXyz {
 		const b: number = parseInt((<HTMLInputElement>document.getElementById('b-input')).value) / 100;
 		const w: number = parseInt((<HTMLInputElement>document.getElementById('w-input')).value) / 100;
 
+		/**
+		 * Sends updated value back to DOM
+		 */
+		document.getElementById('r-value').innerHTML = Math.ceil(r * 100).toString();
+		document.getElementById('g-value').innerHTML = Math.ceil(g * 100).toString();
+		document.getElementById('b-value').innerHTML = Math.ceil(b * 100).toString();
+		document.getElementById('w-value').innerHTML = Math.ceil(w * 100).toString();
+
 		return [r, g, b, w];
 	};
 }
