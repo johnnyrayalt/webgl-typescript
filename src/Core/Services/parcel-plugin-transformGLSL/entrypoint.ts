a0f807468483c0956f8e3cf8e94f4906308e4fc4
Johnny Ray Alt <johnnyrayalt@gmail.com>
2019-12-05 19:43:51 -0800

    removed

--- src/Core/Services/parcel-plugin-transformGLSL/entrypoint.ts
+++
@@ -1,7 +0,0 @@
-export const transformGLSL = (bundler: any): void => {
-	bundler.addAssetType('glsl', require.resolve('./GLSLAsset'));
-	bundler.addAssetType('vert', require.resolve('./GLSLAsset'));
-	bundler.addAssetType('frag', require.resolve('./GLSLAsset'));
-	bundler.addAssetType('vs', require.resolve('./GLSLAsset'));
-	bundler.addAssetType('fs', require.resolve('./GLSLAsset'));
-};
