2e341daa693025e1ce7374dbc19d58ff043e19f9
Johnny Ray Alt <johnnyrayalt@gmail.com>
2019-12-05 18:22:35 -0800

    comments

--- src/Core/Services/InputReferences.ts
+++ src/Core/Services/InputReferences.ts
@@ -1,7 +1,14 @@
+/**
+ * Service for binding and holding values from HTML inputs
+ */
 export class InputReferences {
 	rgbw: { [name: string]: number } = {};
 	sliderBindingsManager: { [name: string]: { input: HTMLInputElement; output: HTMLElement } };
 
+	/**
+	 * Creates an InputReference class to handle HTML bindings
+	 * @this rgbw | Sets default color values to r, g, b = 50 & w = 100.
+	 */
 	constructor() {
 		this.rgbw = { r: 50 / 100, g: 50 / 100, b: 50 / 100, w: 100 / 100 };
 		this.bindSliders();
@@ -9,12 +16,19 @@ export class InputReferences {
 		this.setRGBWValues();
 	}
 
+	/**
+	 * Sets innerString for slider output div
+	 */
 	public setDOMSliderValues = (): void => {
 		Object.keys(this.sliderBindingsManager).forEach((key: string) => {
 			this.sliderBindingsManager[key].output.innerHTML = String(Math.floor(this.rgbw[key] * 100));
 		});
 	};
 
+	/**
+	 * Binds slider input and output HTMLInputElement and HTMLElement respectively
+	 * set @this sliderBindingsManager
+	 */
 	private bindSliders = (): void => {
 		const r: HTMLInputElement = <HTMLInputElement>document.getElementById('r-input');
 		const g: HTMLInputElement = <HTMLInputElement>document.getElementById('g-input');
@@ -34,6 +48,9 @@ export class InputReferences {
 		};
 	};
 
+	/**
+	 * Adds event listener to each slider input to listen for value changes
+	 */
 	private setRGBWValues = (): void => {
 		Object.keys(this.sliderBindingsManager).forEach((key: string) => {
 			this.sliderBindingsManager[key].input.addEventListener('input', (e: any): void => {
