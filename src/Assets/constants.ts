362f4f16a987ea3f7e5025a1f6933542b31638d5
Johnny Ray Alt <johnnyrayalt@gmail.com>
2019-12-06 15:18:59 -0800

    add schema for extending UI creation

--- src/Assets/constants.ts
+++ src/Assets/constants.ts
@@ -1,4 +1,5 @@
-const constants = {
+import { Schema } from '~/Assets/Schema';
+const constants: Schema = {
 	shaders: {
 		type: {
 			vertexShader: 'vertex-shader',
@@ -12,65 +13,81 @@ const constants = {
 	},
 
 	UI: {
-		colorSliders: {
-			containerID: 'slider-container',
-			slider: {
-				redValue: {
-					id: 'r',
-					min: 0,
-					step: 1,
-					max: 100,
-					value: 50,
-				},
-				blueValue: {
-					id: 'g',
-					min: 0,
-					step: 1,
-					max: 100,
-					value: 50,
-				},
-				greenValue: {
-					id: 'b',
-					min: 0,
-					step: 1,
-					max: 100,
-					value: 50,
-				},
-				wValue: {
-					id: 'w',
-					min: 0,
-					step: 1,
-					max: 100,
-					value: 100,
+		sliderGroup: {
+			colors: {
+				containerID: 'slider-container',
+				slider: {
+					redValue: {
+						id: 'r',
+						min: 0,
+						step: 1,
+						max: 100,
+						value: 50,
+					},
+					blueValue: {
+						id: 'g',
+						min: 0,
+						step: 1,
+						max: 100,
+						value: 50,
+					},
+					greenValue: {
+						id: 'b',
+						min: 0,
+						step: 1,
+						max: 100,
+						value: 50,
+					},
+					wValue: {
+						id: 'w',
+						min: 0,
+						step: 1,
+						max: 100,
+						value: 100,
+					},
 				},
 			},
 		},
-		shaderDropdown: {
-			containerID: 'dropdown-container',
-			dropdown: {
-				vertex: {
-					name: 'Vertex Shaders',
-					shader: {
-						get type(): string {
-							return constants.shaders.type.vertexShader;
+		dropdownGroup: {
+			shaders: {
+				containerID: 'dropdown-container',
+				dropdown: {
+					vertex: [
+						{
+							displayName: 'Vertex Shaders',
 						},
-						name: 'Basic Vertex Shader',
-						get path(): string {
-							return constants.shaders.value.basicVertexShader;
+						{
+							shader: {
+								name: 'Basic Vertex Shader',
+								options: {
+									get type(): string {
+										return constants.shaders.type.vertexShader;
+									},
+									get path(): string {
+										return constants.shaders.value.basicVertexShader;
+									},
+								},
+							},
 						},
-					},
-				},
-				fragment: {
-					name: 'Fragment Shaders',
-					shader: {
-						get type(): string {
-							return constants.shaders.type.fragmentShader;
+					],
+					fragment: [
+						{
+							displayName: 'Fragment Shaders',
 						},
-						name: 'Basic Fragment Shader',
-						get path(): string {
-							return constants.shaders.value.basicFragmentShader;
+						{
+							shader: {
+								name: 'Basic Fragment Shader',
+								options: {
+									get type(): string {
+										return constants.shaders.type.fragmentShader;
+									},
+									get path(): string {
+										return constants.shaders.value.basicFragmentShader;
+									},
+								},
+							},
 						},
-					},
+					],
 				},
 			},
 		},
