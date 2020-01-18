b75af8d37a1bdc95b9bab9bc229a5a642fabc91f
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-01-17 17:19:29 -0800

    move draw and loop methods to render class

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,10 @@ import VertexShader from '~/Core/Shaders/vertex.vert';
 import { indices } from './Core/Geometry/indices';
 import { vertices } from './Core/Geometry/vertices';
 import { GLProgram } from './Core/GLComponents/GLProgram/GLProgram';
+import { GLRender } from './Core/GLComponents/GLRender';
 
-const projectionMatrix = mat4.create();
-const modelViewMatrix = mat4.create();
+const projectionMatrix: mat4 = mat4.create();
+const modelViewMatrix: mat4 = mat4.create();
 
 const canvas = new GLCanvas();
 const gl = canvas.getGLContext();
@@ -19,60 +20,17 @@ canvas.clearColor([0, 0, 0, 1]);
 // WebGL2 Application Setup
 
 // Set up state
-const glProgram = new GLProgram(gl, VertexShader, FragmentShader).getGLProgram();
+const glProgram: WebGLProgram = new GLProgram(gl, VertexShader, FragmentShader).getGLProgram();
 
-// // Attributes & Uniforms
+// // Attributes
 const aVertexPosition = gl.getAttribLocation(glProgram, 'aVertexPosition');
-// const uModelViewMatrix = gl.getUniformLocation(glProgram, 'uModelViewMatrix');
-// const uProjectionMatrix = gl.getUniformLocation(glProgram, 'uProjectionMatrix');
 
 const positionBuffer = new GLBuffer(gl, vertices, indices, aVertexPosition);
-
-const draw = (): void => {
-	// clear the scene
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-	mat4.perspective(projectionMatrix, 45, gl.canvas.width / gl.canvas.height, 0.1, 10000);
-	mat4.identity(modelViewMatrix);
-	mat4.translate(modelViewMatrix, modelViewMatrix, [0, 0, -5]);
-
-	const numUniforms = gl.getProgramParameter(glProgram, gl.ACTIVE_UNIFORMS);
-	let uniformIndex: number;
-	let uniformMatrixMap: { [name: string]: WebGLUniformLocation } = {};
-	for (uniformIndex = 0; uniformIndex < numUniforms; uniformIndex++) {
-		const uniformInfo: WebGLActiveInfo = gl.getActiveUniform(glProgram, uniformIndex);
-		if (!uniformInfo) {
-			break;
-		}
-		uniformMatrixMap[uniformInfo.name] = gl.getUniformLocation(glProgram, uniformInfo.name);
-	}
-
-	// TODO:: Less hardcodedness please
-	Object.keys(uniformMatrixMap).forEach((name: string) => {
-		if (name === 'uProjectionMatrix') gl.uniformMatrix4fv(uniformMatrixMap[name], false, projectionMatrix);
-		if (name === 'uModelViewMatrix') gl.uniformMatrix4fv(uniformMatrixMap[name], false, modelViewMatrix);
-	});
-
-	// Bind VAO
-	gl.bindVertexArray(positionBuffer.getVAO());
-
-	// bind ibo
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, positionBuffer.getIndicesBufferObject());
-
-	// draw to the scene using triangle primitives
-	gl.drawElements(gl.LINE_LOOP, indices.length, gl.UNSIGNED_SHORT, 0);
-
-	// clean up
-	gl.bindVertexArray(null);
-	gl.bindBuffer(gl.ARRAY_BUFFER, null);
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-};
 
 const start = (): void => {
 	canvas.resize();
 	canvas.clearColor([0, 0, 0, 1]);
-	draw();
+	new GLRender(gl, glProgram, positionBuffer, indices, projectionMatrix, modelViewMatrix).loop();
 };
 
 ((): void => {
